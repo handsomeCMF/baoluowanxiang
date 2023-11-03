2a421225e42257c571b40daf1c066d1511dcf5e7
victor.cai <victor.cai@hkbn.com.hk>
2023-11-03 15:33:38 +0800

    code splitting setting

--- scripts/webpack.pod.js
+++ scripts/webpack.pod.js
@@ -1,6 +1,30 @@
 const base = require('./webpack.base.js');
+const TerserPlugin = require("terser-webpack-plugin");
 const { merge } = require('webpack-merge');
+const path = require('path');
 
 module.exports = merge(base, {
   mode: 'production',
+  entry: {
+    index: path.resolve(__dirname, '../index.tsx'),
+    another: path.resolve(__dirname, '../another-module.tsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    // webpack已经弃用Uglify了 改用 terser-webpack-plugin
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: { warnings: false }
+    // }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+    splitChunks: {
+      // code splitting -- https://webpack.js.org/guides/code-splitting/
+      chunks: 'all',
+    }
+  }
 })
