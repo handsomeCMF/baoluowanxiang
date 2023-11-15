aea4ebaf00ca28b368331c7e10266c6cc17672b1
victor.cai <victor.cai@hkbn.com.hk>
2023-11-15 09:01:22 +0800

    redux use demo

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import './App.less';
 import leiShen from './assets/image/2.jpg';
 import XiaoCaoShen from './assets/image/1.jpg';
 import { Tabs, Box, Tab, Typography } from '@mui/material';
-import { connect, ConnectedProps } from 'react-redux';
+import { useAppDispatch, useAppSelector } from './utils/hooks';
 import { RootState } from './store/store';
+import { changeTab } from './store/gameTab/tabSlice';
 
 const mhyGameList : Array<{ label: string }> = [
   { label: '原神' },
@@ -39,36 +40,20 @@ function CustomTabPanel(props: TabPanelProps) {
   );
 }
 
-const mapState = (state: RootState) => ({
-  tab: state.tabReducer.selectedTab
-});
-
-const mapDispatch = {
-  changeTab: (payload: number) => ({ type: "changeTab", payload })
-};
-
-const connector = connect(mapState, mapDispatch);
-
-type PropsFromRedux = ConnectedProps<typeof connector>;
-
-interface Props extends PropsFromRedux {
-  // selectedTab: number,
-}
-
-const App = (props: Props) => {
-  const { tab } = props;
+const App = () => {
   // const [selectedTab, setSelectedTab] = React.useState(0);
+  const tab = useAppSelector((state: RootState) => state.tabReducer.selectedTab);
+  const dispatch = useAppDispatch();
 
   const handleChangeTab = (event: React.SyntheticEvent, newValue: number) => {
-    console.log(event, newValue);
-    props.changeTab(newValue);
+    dispatch(changeTab(newValue));
   };
   return (
     <div>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={tab} onChange={handleChangeTab} aria-label='米哈游菜单'>
           {mhyGameList.map(gameItem => (
-            <Tab key={gameItem.label} label={gameItem.label} />
+            <Tab label={gameItem.label} key={gameItem.label} />
           ))}
         </Tabs>
       </Box>
@@ -76,7 +61,7 @@ const App = (props: Props) => {
       <CustomTabPanel value={tab} index={0}>
         <h1>hello 原神</h1>
         <img src={leiShen} width={900} height={400}/>
-        <img src={XiaoCaoShen} width={900} height={400}/>
+        <img src={XiaoCaoShen} width={900} height={400}/> --小草神比雷神出来的更快
       </CustomTabPanel>
       <CustomTabPanel value={tab} index={1}>
         <h1>hello 崩坏·星穹铁道</h1>
@@ -91,4 +76,4 @@ const App = (props: Props) => {
   )
 }
 
-export default connector(App);
+export default App;
